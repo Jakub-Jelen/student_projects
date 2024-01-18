ceb391c3eb8bee0e065293c8855a81d8b2525764
Jakub-Jelen <jakub.jelen@e-zstih.edu.pl>
2024-01-18 13:54:39 +0000

    studentmachine send

---
+++ cpp/cwiczenie2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int x;
+cout<<"podaj wiek";
+cin>>x;
+if (x>=18){
+cout<<"jestes dorosły";
+}
+else{
+cout<<"wypad mlody";
+}
+return 0;
+}
