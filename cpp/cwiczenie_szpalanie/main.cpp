0a6414f8e4a4be99740726dddf7efd5c27125074
Jakub-Jelen <jakub.jelen@e-zstih.edu.pl>
2024-02-22 14:01:51 +0000

    studentmachine send

---
+++ cpp/cwiczenie_szpalanie/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main(){
+using namespace std;
+int osoby;
+float odleglosc;
+float spalanie;
+float paliwo;
+int cena;
+cout<<"ile osób jedzie?"<<endl;
+cin>>osoby;
+cout<<"jaka odległość została przejechana"<<endl;
+cin>>odleglosc;
+cout<<"jakie było średnie spalanie?"<<endl;
+cin>>spalanie;
+cout<<"ile paliwa można zatankować?"<<endl;
+cin>>paliwo;
+cout<<"jaka cena paliwa?"<<endl;
+cin>>cena;
+cout<<"ile zużyjesz paliwa";
+return 0;
+}
