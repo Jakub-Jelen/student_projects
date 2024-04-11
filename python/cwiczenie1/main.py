a60682ceac2ceb346118457dc04e22938e872213
Jakub-Jelen <jakub.jelen@e-zstih.edu.pl>
2024-04-11 13:06:26 +0000

    studentmachine send

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,13 @@
+print ("witaj swiecie")
+for z in range(1,10):
+	print (z)
+y=float(input("podaj wartosc y"))
+x=float(input("podaj wartosc x"))
+if x>10:
+	print("większy 10")
+elif x==10 and y==20:
+	print("suganigadick")
+else:
+	print("mniejszy 10")
+print (y+x)
+
